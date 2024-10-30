a2744e1f17d600a9ea43a983dabf351b14e5753d
Daniel Jin <mrpickles123115@gmail.com>
2024-10-29 23:24:09 -0600

    add buggy scanner implementation

--- calculator.c
+++ calculator.c
@@ -10,15 +10,34 @@ int main() {
 
 	stdio_init_all();
 
-	gpio_init(LED_PIN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
+	gpio_init_mask(0x0001ffff);
+	for (int i = 0; i < 17; i++) {
+		gpio_pull_up(i);
+	}
+
+	// First 17 pins
+
 
 	while (1) {
-		gpio_put(LED_PIN, 0);
-		sleep_ms(250);
-		gpio_put(LED_PIN, 1);
-		puts("Hello World\n");
-		sleep_ms(1000);
+		for (int i = 0; i < 16; i++) {
+			for (int j = i + 1; j < 17; j++) {
+				//read pin always registers on multiple i's for some reason, assuming because switching i from OUT to IN takes longer then IN to OUT
+				gpio_set_dir(j, GPIO_IN);
+				gpio_set_dir(i, GPIO_OUT);
+
+				
+				gpio_put(i, 0);
+
+				bool v = gpio_get(j);
+				gpio_set_dir(i, GPIO_IN);
+				if (!v) {
+					//avoid flooding screen smfh
+					sleep_ms(50);
+					printf("Hit! at %d, %d\n", i,  j);
+				}
+
+			}
+		}
 	}
 }
 	
