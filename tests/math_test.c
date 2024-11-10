015b1afb3821547336149b022e9512ccc94b0533
Daniel Jin <mrpickles123115@gmail.com>
2024-11-10 15:25:34 -0700

    encoding perserves digits but exponents are messed up

--- tests/math_test.c
+++ tests/math_test.c
@@ -2,9 +2,52 @@
 #include <stdint.h>
 #include "libcalc.h"
 
-void uint16_tbs(uint16_t num, char* arr, int length) {
+void describe_dec128(dec128 * src) {
+	printf("Value: ");
+	if (src->sign) {
+		putchar('-');
+	}
+	for (int i = 0; i < DEC128_BCDBYTES; i++) {
+		uint8_t a, b;
+		a = src->digits[i] >> 4;
+		b = src->digits[i] & 0b1111;
+		putchar('0' + a);
+		putchar('0' + b);
+	}
+	printf(" * 10^%d [%d]\n", src->exponent, src->flags);
+
+}
+
+void u32tb(uint32_t num, char* arr) {
+	for (int i = 0; i < 32; i++) {
+		int j = (32 - 1) - i;
+		if (num & (1 << i)) {
+			arr[j] = '1';
+		} else {
+			arr[j] = '0';
+		}
+	}
+}
+
+void describe_decimal128(decimal128* src) {
+	uint8_t a, b, c;
+	char buf[33] = {0};
+	buf[32] = 0;
+	a = src->data[0];
+	b = src->data[1];
+	c = src->data[2];
+	printf("sign: %d\n", a >> 7);
+	uint32_t combined;
+	combined = (a & 0b1111111) << 10;
+	combined = combined | (b << 2);
+	combined = combined | (c >> 6);
+	u32tb(combined, buf);
+	printf("combined: %s\n", buf + 15);
+}
+
+void u16tb(uint16_t num, char* arr) {
 	for (int i = 0; i < 16; i++) {
-		int j = (length - 1) - i;
+		int j = (16 - 1) - i;
 		if (num & (1 << i)) {
 			arr[j] = '1';
 		} else {
@@ -23,7 +66,7 @@ int main(int argc, char** argv) {
 		for (int j = 0; j < 10; j++) {
 			for (int k =0; k < 10; k++) {
 				uint16_t dpd = encode_dpd(i, j, k);
-				uint16_tbs(dpd, bs, 16);
+				u16tb(dpd, bs);
 				uint8_t a, b, c;
 				decode_dpd(dpd, &a, &b, &c);
 				if ((a != i) || (b != j) || (c != k)) {
@@ -42,6 +85,31 @@ int main(int argc, char** argv) {
 	if (dpd_pass) {
 		printf("[*] DPD passed!\n");
 	}
+
+	printf("[*] Testing dec encodings\n");
+	dec128 pi = {
+		.sign = 0,
+		.exponent = -33,
+		.flags = 0,
+		.digits = {0x31, 0x41, 0x59, 0x26, 0x53, 0x58, 0x97, 0x93, 0x23, 0x84, 0x62, 0x64, 0x33, 0x83, 0x27, 0x95, 0x02}
+	};
+	describe_dec128(&pi);
+	dec128 big = {
+		.sign = 0,
+		.exponent = 0,
+		.flags = 0,
+		.digits = {0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99}
+	};
+	describe_dec128(&big);
+
+	decimal128 bigpi;
+	dec128 newpi;
+
+	pack_decimal128(&pi, &bigpi);
+	unpack_decimal128(&bigpi, &newpi);
+
+	describe_decimal128(&bigpi);
+	describe_dec128(&newpi);
 
 
 	return 0;
